92a43dd88eaa9d79193d361beaff036515cbcda2
Kexin Xie <kexin.xie@salesforce.com>
2020-04-10 17:06:55 -0700

    Everything I've got so far

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # My neovim setup
+
+## Setup
+
+Run `./bootstrap.sh`, which will perform the following steps:
+
+    1. Download vim-plug into the right location
+    1. Creating the directory structure for nvim
+    1. Linking the configuration file in this repo to the right path
+
+Next launch `nvim` and type `:PlugInstall` to install all the configured
+plugins
---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+project_root="$(cd "`dirname "$0"`"; pwd)"
+
+echo "Installing vim-plug..."
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Creating nvim config directory..."
+mkdir -p ~/.config/nvim/
+
+echo "Linking init.vim file..."
+init_vim_path="nvim/init.vim"
+rm ~/.config/$init_vim_path
+ln $project_root/dot-config/$init_vim_path ~/.config/$init_vim_path
