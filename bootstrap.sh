570f24cf86253bd9baf2479ebca9ea1a9d6d5665
Kexin Xie <kexin.xie@salesforce.com>
2021-03-27 20:52:53 -0700

    Include vim setup too

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,12 @@
 
 project_root="$(cd "`dirname "$0"`"; pwd)"
 
-echo "Installing vim-plug..."
+echo "Installing vim-plug for neovim..."
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Installing vim-plug for vim..."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Creating nvim config directory..."
@@ -13,3 +17,6 @@ echo "Linking init.vim file..."
 init_vim_path="nvim/init.vim"
 rm -rf ~/.config/$init_vim_path
 ln $project_root/dot-config/$init_vim_path ~/.config/$init_vim_path
+
+echo "Linking .vimrc file..."
+ln $project_root/dot_vimrc ~/.vimrc
