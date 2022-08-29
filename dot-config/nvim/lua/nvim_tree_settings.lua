421afeb3618f80e2cb9ab5b26a18b80a5adfa3c7
Kexin Xie <kexin.xie@salesforce.com>
2022-08-29 12:07:52 -0700

    configure nvim-tree key bindings

--- dot-config/nvim/lua/nvim_tree_settings.lua
+++ dot-config/nvim/lua/nvim_tree_settings.lua
@@ -1,6 +1,12 @@
 require("nvim-tree").setup({
   view = {
     adaptive_size = true,
+    mappings = {
+      list = {
+        { key = { "<CR>", "o" }, action = "edit_no_picker" },
+        { key = "i", action = "vsplit" },
+      },
+    }
   },
   filters = { custom = { "^.git$" } },
 })
