c60022e2527d42dec883c01dbc588273b09abcd3
Kexin Xie <kexin.xie@salesforce.com>
2022-11-29 20:24:15 -0800

    display relative path for file names

--- dot-config/nvim/lua/lualine_settings.lua
+++ dot-config/nvim/lua/lualine_settings.lua
@@ -1,5 +1,10 @@
 require('lualine').setup {
   options = {
     theme = 'nord'
+  },
+  sections = {
+    lualine_c = {
+      {'filename', path = 1} -- 0 = just name, 1 = relative path, 2 = absolute path
+    }
   }
 }
