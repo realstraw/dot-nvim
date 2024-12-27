22e17fb53cb88033e969b2d06ba10cb408fdcf81
Kexin Xie <realstraw@gmail.com>
2024-12-27 15:37:15 -0800

    comment out packer and vim-plug from bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,14 +9,14 @@ echo "Linking nvim directory..."
 rm -rf ~/.config/nvim
 ln -s $project_root/dot-config/nvim ~/.config/nvim
 
-echo "Installing vim-plug for vim..."
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-echo "Installing packer.nvim for vim..."
-rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# echo "Installing vim-plug for vim..."
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# 
+# echo "Installing packer.nvim for vim..."
+# rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+#      ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 echo "Linking .vimrc file..."
 rm ~/.vimrc
