08c6857bf3bd6225f4626c7441c3b8ff81081ba3
Kexin Xie <realstraw@gmail.com>
2026-04-18 12:26:21 -0700

    add neovide settings and update dependencies

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -8,3 +8,12 @@ vim.cmd.colorscheme("nord")
 -- vim.cmd.colorscheme("e-ink")
 
 vim.filetype.add({ extension = { mdx = 'markdown' } })
+
+vim.o.guifont = "Hack Nerd Font Mono:h12"
+
+if vim.g.neovide then
+  -- disable animation
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_position_animation_length = 0
+  vim.g.neovide_scroll_animation_length = 0
+end
